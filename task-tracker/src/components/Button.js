144c6e24e7f805a940e6e69ddd7f3c6df5cfef23
Bluemace <36447393+Bluemace@users.noreply.github.com>
2021-06-07 19:22:49 +0200

    Added components and logic

--- task-tracker/src/components/Button.js
+++ task-tracker/src/components/Button.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+const Button = ({color, text, onClick}) => {
 
-const Button = ({color, text}) => {
-    return <button style = {{backgroundColor: color}} className = 'btn'>{text}</button>
-    
+
+    return( 
+    <button onClick = {onClick} style = {{backgroundColor: color}} className = 'btn'>{text}</button>
+    )
 }
 
+Button.defaultProps = {
+    color:'steelblue'
+}
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onClick: PropTypes.func.isRequired,
+}
 export default Button
